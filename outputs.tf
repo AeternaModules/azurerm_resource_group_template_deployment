d7aa094eca3bd712962e0ce72f408dab83a2ba2f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:59:38 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "resource_group_template_deployments" {
+  description = "All resource_group_template_deployment resources"
+  value       = azurerm_resource_group_template_deployment.resource_group_template_deployments
+}
+
